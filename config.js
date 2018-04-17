af3180482058633d738e22c2d5e4c11d4e355b66
Antonio Glavocevic <tony@it-tony-mac.local>
2018-04-17 13:59:05 +1000

    different machines without swarm

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ var hfc = require('fabric-client');
 // to have the hfc load it to initalize the fabric client instance
 hfc.setConfigSetting('network-connection-profile-path',path.join(__dirname, 'artifacts', 'network-config.yaml'));
 hfc.setConfigSetting('Org1-connection-profile-path',path.join(__dirname, 'artifacts', 'org1.yaml'));
+hfc.setConfigSetting('Org2-connection-profile-path',path.join(__dirname, 'artifacts', 'org2.yaml'));
 // some other settings the application might need to know
 hfc.addConfigFile(path.join(__dirname, 'config.json'));
